b997074b8e50901d422d6036a7525907cb374b8f
William J. Bowman <wjb@williamjbowman.com>
2018-06-29 23:01:53 -0400

    Removed cpdf and pdfsizeopt from shrink-pdf.sh

--- shrink-pdf.sh
+++ shrink-pdf.sh
@@ -1,44 +1,27 @@
 #!/bin/sh
 
-
 # screen < ebook < print
 QUALITY=${QUALITY-ebook}
 
-OUT="$1"
-INPUT="$1.preshrink.pdf"
+IN="$1"
+OUT=`mktemp`
 JOBS=${JOBS-2}
-
-if [ ! -e "$INPUT" ]; then
-   cp "$OUT" "$INPUT"
-fi
 
 shift
 
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 \
    -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages \
    -dSubsetFonts=true -dEmbedAllFonts=true -dPDFFITPAGE -dColorConversionStrategy=/LeaveColorUnchanged \
-   -dCompressFonts=true -dPDFSETTINGS=/$QUALITY $@ -sOutputFile="$OUT" "$INPUT"
+   -dCompressFonts=true -dPDFSETTINGS=/$QUALITY $@ -sOutputFile="$OUT" "$IN"
 
-if which cpdf; then
-    TMP=`mktemp`
-    cp "$OUT" "$TMP"
-    cpdf -squeeze "$TMP" AND -compress AND -clean -o "$OUT"
-    rm "$TMP"
-fi
-
-if which pdfsizeopt; then
-    TMP=`mktemp`
-    cp "$OUT" "$TMP"
-    pdfsizeopt --use-image-optimizer="ect -9 -strip -progressive --mt-deflate=$JOBS %(targetfnq)s" "$TMP" "$OUT"
-    rm "$TMP"
-fi
-
-SIZE1=`stat -c"%s" "$INPUT"`
+SIZE1=`stat -c"%s" "$IN"`
 SIZE2=`stat -c"%s" "$OUT"`
 PERC=`bc <<< "scale=2; ($SIZE2 - $SIZE1)/$SIZE1 * 100"`
 if [[ $SIZE2 -ge $SIZE1 ]]; then
     echo "Didn't shrink; reverting."
-    mv "$INPUT" "$OUT"
+    rm "$OUT"
 else
-    echo "Saved $PERC %; try manually spot-checking $OUT"
+    mv "$IN" "$IN.preshrink.pdf"
+    mv "$OUT" "$IN"
+    echo "Saved $PERC %. Original saved as \"$IN.preshrink.pdf\". Try manually spot-checking before removing."
 fi
