d642c531f832bb7b3bb6870f0b75fcebbe2e1db5
William J. Bowman <wjb@williamjbowman.com>
2018-06-30 00:42:39 -0400

    Comments, and removed JOBS
    Let the software figure out how many jobs to use

--- sizeopt.sh
+++ sizeopt.sh
@@ -2,9 +2,10 @@
 
 IN=$1
 OUT=`mktemp`
-JOBS=${JOBS-2}
 
-/usr/bin/pdfsizeopt --use-jbig2=yes --use-image-optimizer="ect -9 -strip -progressive --mt-deflate=$JOBS %(targetfnq)s" --tmp-dir=/tmp "$IN" "$OUT"
+# jbig2 makes a big difference.
+# ect makes a difference, and is faster than pngout
+/usr/bin/pdfsizeopt --use-jbig2=yes --use-image-optimizer="ect -9 -strip -progressive --mt-deflate %(targetfnq)s" --tmp-dir=/tmp "$IN" "$OUT"
 
 SIZE1=`stat -c"%s" "$IN"`
 SIZE2=`stat -c"%s" "$OUT"`
