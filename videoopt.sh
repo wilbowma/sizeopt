83d1face952b7003a8411e51da70326e01bd919b
William J. Bowman <wjb@williamjbowman.com>
2018-07-18 02:17:48 -0400

    More docs for videoopt

--- videoopt.sh
+++ videoopt.sh
@@ -2,20 +2,24 @@
 
 echo "You may not not this script until you manually remove the following"
 echo "exit."
-echo "By default, this script does lossy-to-lossy audio transcoding, which is"
+echo "By default, this script does lossy-to-lossy transcoding, which is"
 echo "Bad (TM)."
-# However, in practice, it has halved the size of my videos with no noticable
-# loss in quality.
-# NB: Audio optimize for voice; remove -application voip for other and consider
+# However, in practice, I haven't noticed significant loss of quality in the
+# videos.
+# Audio optimize for voice; remove -application voip for other and consider
 # adjusting bitrate (-b:a)
+# Adjust -b:a to improve audio equality, or change set -c:a copy
+# Adjust -crf to improve video quality.
+# 34 is a pretty low quality factor... but I haven't noticed anything yet.
+# 23 is the default, and 17 is consider visually lossless. 0 is lossless.
+# Adjust PRESENT if you want worse, but faster, compression.
+# Anecdata suggests that actually veryfast is the best speed/compression
+# ratio... but I only transcode things once, while many people may download
+# them.
 exit
 
 IN="$1"
 OUT=`mktemp`
-# configure this if you want worse, but faster, compression
-# anecdata suggests that actually veryfast is the best speed/compression
-# ratio... but I only transcode things once, while many people may download
-# them.
 PRESET=veryslow
 
 EXTENSION="${IN##*.}"
