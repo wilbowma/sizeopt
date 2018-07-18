b2efc51380357342511dcf2f6acde55738934213
William J. Bowman <wjb@williamjbowman.com>
2018-07-18 01:56:56 -0400

    Add disclaimer

--- videoopt.sh
+++ videoopt.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+echo "You may not not this script until you manually remove the following"
+echo "exit."
+echo "By default, this script does lossy-to-lossy audio transcoding, which is"
+echo "Bad (TM)."
+# However, in practice, it has halved the size of my videos with no noticable
+# loss in quality.
+# NB: Audio optimize for voice; remove -application voip for other and consider
+# adjusting bitrate (-b:a)
+exit
+
 IN="$1"
 OUT=`mktemp`
 # configure this if you want worse, but faster, compression
@@ -7,8 +17,6 @@ PRESET=veryslow
 
 EXTENSION="${IN##*.}"
 
-# NB: Audio optimize for voice; remove -application voip for other and consider
-# adjusting bitrate (-b:a)
 ffmpeg -i "$IN" -c:v libx265 -preset $PRESET -crf 34 -c:a libopus -vbr on -compression_level 10 -b:a 48k -application voip "$OUT.mkv"
 
 SIZE1=`stat -c"%s" "$IN"`
